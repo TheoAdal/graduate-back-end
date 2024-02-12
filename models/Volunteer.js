374eaecae4c02ffd660fddec6689b478c5da0beb
TheoAdal <theoadal@gmail.com>
2024-02-12 18:01:32 +0200

    back end lolo

---
+++ models/Volunteer.js
@@ -0,0 +1,11 @@
+// models/Volunteer.js
+const mongoose = require('mongoose');
+
+const volunteerSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  // Admin-specific properties
+});
+
+module.exports = mongoose.model('Volunteer', volunteerSchema);
